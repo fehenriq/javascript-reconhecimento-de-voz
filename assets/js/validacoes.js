bb94feb43c8fbd49e959c2753f9b30cc9e473826
ti7 <ti7@gimipogliano.local>
2022-09-09 10:48:49 -0300

    game ready to play

---
+++ assets/js/validacoes.js
@@ -0,0 +1,54 @@
+function verificaChute() {
+    const numero = +chute
+
+    if (chuteInvalido(numero)) {
+        elementoChute.innerHTML += ('<div>Valor inválido</div>')
+        return
+    }
+
+    if (numeroMaiorMenor(numero)) {
+        elementoChute.innerHTML += (`<div>Fale um número entre ${menorValor} e ${maiorValor}</div>`)
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O numero secreto era ${numeroSecreto}</h3>
+            <button class="botao-jogar" type="button" onClick="jogarNovamente()">JOGAR NOVAMENTE</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
+    } else {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>`
+    }
+
+    if (chute.toUpperCase() === "GAME OVER") {
+        document.body.innerHTML = `
+            <h2>Game Over!!!</h2>
+            <button class="botao-jogar" type="button" onClick="jogarNovamente()">JOGAR NOVAMENTE</button>
+        `
+            document.body.style.backgroundColor = "black";
+    } else {
+        elementoChute.innerHTML += '<div>Valor Inválido</div>';
+    }
+
+    if (chute === "Fran") {
+        document.body.innerHTML = `
+            <h1>É o amor da minha vida!</h1>
+            <button class="botao-jogar" type="button" onClick="jogarNovamente()">JOGAR NOVAMENTE</button>
+        `
+    }
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroMaiorMenor(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+function jogarNovamente(){
+    window.location.reload();
+} 
